513d69e4ce30d2daabd418dc3dfedfaa07127271
Kiet Lam <kmlam1375@gmail.com>
2022-04-20 14:23:23 +1000

    include masterpage features to the html pages

--- agentusermanagement/settings.py
+++ agentusermanagement/settings.py
@@ -124,4 +124,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = "/static/"
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
+
 django_heroku.settings(locals())
+
