3606cc969ced5b9c9743b13593a7b41d92a146bd
Kiet Lam <kmlam1375@gmail.com>
2022-04-21 01:43:32 +1000

    cleaned up Crypto_Bytes

--- IAM/SecretsManager/Crypto_Bytes.py
+++ IAM/SecretsManager/Crypto_Bytes.py
@@ -1,13 +1,5 @@
 import secrets, random
 
-class Crypto_Bytes():
-    key =''
-
-
-def checkKey(key):
-    if key > 0:
-        return True
-
 def generateSecret():
     secretsGenerator = secrets.SystemRandom()
     array = [0] * 256
